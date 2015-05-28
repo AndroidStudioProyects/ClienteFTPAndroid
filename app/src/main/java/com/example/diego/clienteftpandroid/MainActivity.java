7e738768792fe433505d2ec6e3e4e8e7c6a08ab9
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2015-05-28 18:16:44 -0300

    funcional local

--- app/src/main/java/com/example/diego/clienteftpandroid/MainActivity.java
+++ app/src/main/java/com/example/diego/clienteftpandroid/MainActivity.java
@@ -1,31 +1,33 @@
 package com.example.diego.clienteftpandroid;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.File;
 
 
 public class MainActivity extends ActionBarActivity {
-    connnectingwithFTP cliente;
+   // connnectingwithFTP cliente;
+    ConnectUploadAsync cliente;
     uploadFile subir;
     downloadSingleFile bajarArchivo;
     Button btn_Subir, btn_Conectar;
+    EditText edit_ServerFtp,edit_Puerto,edit_User,edit_Pass;
     File Archivo;
-String ip="192.168.0.103",userName="idirect",pass="IDIRECT";
+    TextView text_Bytes;
+  String userName,pass,ip;
 
-    /*********  work only for Dedicated IP ***********/
-    static final String FTP_HOST= "192.168.0.103";
-
-    /*********  FTP USERNAME ***********/
-    static final String FTP_USER = "idirect";
-
-    /*********  FTP PASSWORD ***********/
-    static final String FTP_PASS  ="IDIRECT";
+    ProgressBar progressbar;
 
 
 
@@ -35,26 +37,43 @@ String ip="192.168.0.103",userName="idirect",pass="IDIRECT";
         setContentView(R.layout.activity_main);
         levantar_xml();
         Botones();
-
+        CargarPreferencias();
 
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        GuardarPreferencias();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        CargarPreferencias();
+    }
+
     private void Botones() {
+
         btn_Conectar.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
+            //    GuardarPreferencias();
+               ip=edit_ServerFtp.getText().toString();
+                userName=edit_User.getText().toString();
+                pass=edit_Pass.getText().toString();
 
-               if(btn_Conectar.getText().toString().equals("Conectar")){
-                cliente=new connnectingwithFTP(ip,userName,pass);
-                cliente.start();
-                   btn_Conectar.setText("Desconectar");
-               }else{
+            //    cliente=new connnectingwithFTP(ip,userName,pass,getApplicationContext());
+             //   cliente.start();
+           cliente = new ConnectUploadAsync(getApplicationContext(),ip,userName,pass,MainActivity.this);
+                cliente.execute();
 
-
-                   cliente.Desconectar();
-                   btn_Conectar.setText("Conectar");
-               }
             }
         });
 
@@ -70,6 +89,14 @@ String ip="192.168.0.103",userName="idirect",pass="IDIRECT";
     private void levantar_xml() {
         btn_Conectar= (Button) findViewById(R.id.btn_Conectar);
         btn_Subir= (Button) findViewById(R.id.btn_Subir);
+
+        text_Bytes=(TextView)findViewById(R.id.text_Bytes);
+
+        edit_ServerFtp=(EditText)findViewById(R.id.edit_ServerFtp);
+        edit_Puerto=(EditText)findViewById(R.id.edit_Puerto);
+        edit_User=(EditText)findViewById(R.id.edit_User);
+        edit_Pass=(EditText)findViewById(R.id.edit_Pass);
+        progressbar=(ProgressBar)findViewById(R.id.progressBar);
      }
 
 
@@ -94,4 +121,31 @@ String ip="192.168.0.103",userName="idirect",pass="IDIRECT";
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    ///////////////////// PREFERENCIAS DE USUARIO ////////////////
+    public void CargarPreferencias(){
+
+        SharedPreferences mispreferencias=getSharedPreferences("PreferenciasUsuario", Context.MODE_PRIVATE);
+        edit_ServerFtp.setText(mispreferencias.getString("edit_IP", "localhost"));
+        edit_Puerto.setText(mispreferencias.getString("edit_Port", "21"));
+        edit_User.setText(mispreferencias.getString("edit_User", "idirect"));
+        edit_Pass.setText(mispreferencias.getString("edit_Pass", "IDIRECT"));
+        Log.d("Android_FTP", "Preferencias Cargadas");
+
+
+    }
+
+    public void GuardarPreferencias() {
+        SharedPreferences mispreferencias = getSharedPreferences("PreferenciasUsuario", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mispreferencias.edit();
+        editor.putString("edit_IP", edit_ServerFtp.getText().toString());
+        editor.putString("edit_Port", edit_Puerto.getText().toString());
+        editor.putString("edit_User", edit_User.getText().toString());
+        editor.putString("edit_Pass", edit_Pass.getText().toString());
+        editor.commit();
+        Log.d("Android_FTP", "Preferencias Almacenadas");
+
+    }
+// //////////////////////////////////////////////////////////
 }
--- app/src/main/java/com/example/diego/clienteftpandroid/connnectingwithFTP.java
+++ app/src/main/java/com/example/diego/clienteftpandroid/connnectingwithFTP.java
@@ -1,5 +1,7 @@
 package com.example.diego.clienteftpandroid;
 
+import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
 
 import org.apache.commons.net.ftp.FTP;
@@ -7,7 +9,10 @@ import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
@@ -19,12 +24,14 @@ import java.net.UnknownHostException;
  */
 public class connnectingwithFTP extends Thread{
     String ip, userName,pass;
-
+Context contexto;
     FTPClient mFtpClient;
-    public connnectingwithFTP(String ip, String userName, String pass) {
+
+    public connnectingwithFTP(String ip, String userName, String pass,Context contexto) {
      this.ip=ip;
         this.userName=userName;
         this.pass=pass;
+        this.contexto=contexto;
     }
 
     public void Desconectar(){
@@ -50,15 +57,31 @@ public class connnectingwithFTP extends Thread{
             status = mFtpClient.login(userName, pass);
             Log.d("Api FTP","Se conecto: "+ String.valueOf(status));
             if (FTPReply.isPositiveCompletion(mFtpClient.getReplyCode())) {
-                mFtpClient.setFileType(FTP.ASCII_FILE_TYPE);
+                mFtpClient.setFileType(FTP.BINARY_FILE_TYPE);
                 mFtpClient.enterLocalPassiveMode();
 
                 FTPFile[] mFileArray = mFtpClient.listFiles();
                 Log.d("Api FTP","Numeros de archivos"+ String.valueOf(mFileArray.length));
-                Log.d("Api FTP","IP Server:"+  String.valueOf(mFtpClient.getRemoteAddress()));
-                downloadSingleFile baja=new downloadSingleFile();
-                baja.start();
-                           }
+                Log.d("Api FTP", "IP Server:" + String.valueOf(mFtpClient.getRemoteAddress()));
+
+
+                // APPROACH #1: uploads first file using an InputStream
+                File firstLocalFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"Radiobases/Prueba.jpg");
+
+                String firstRemoteFile = "Prueba.jpg";
+                InputStream inputStream = new FileInputStream(firstLocalFile);
+                boolean done = mFtpClient.storeFile(firstRemoteFile, inputStream);
+                inputStream.close();
+                if (done) {
+                    System.out.println("el primer archivo se subio perfecto!!.");
+            //        Toast.makeText(contexto,"Archivo subido...",Toast.LENGTH_SHORT).show();
+                }else{System.out.println("Error de subida");}
+
+
+
+
+
+            }
         } catch (SocketException e) {
             e.printStackTrace();
         } catch (UnknownHostException e) {
